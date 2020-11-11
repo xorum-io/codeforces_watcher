3f593d3d08a640cd847f362a1182a11406f9747a
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2020-11-11 20:55:46 +0200

    #212; Remove CustomMainScope.

--- common/src/commonMain/kotlin/io/xorum/codeforceswatcher/coroutines/CustomMainScope.kt
+++
@@ -1,5 +0,0 @@
-package io.xorum.codeforceswatcher.coroutines
-
-import kotlinx.coroutines.CoroutineScope
-
-internal expect fun CustomMainScope(): CoroutineScope
--- common/src/jvmMain/kotlin/io/xorum/codeforceswatcher/coroutines/CustomMainScope.kt
+++
@@ -1,6 +0,0 @@
-package io.xorum.codeforceswatcher.coroutines
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-
-internal actual fun CustomMainScope(): CoroutineScope = MainScope()
