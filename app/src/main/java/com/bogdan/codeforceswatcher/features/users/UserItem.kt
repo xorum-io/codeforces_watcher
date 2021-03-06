970e9d081baafeadac8f2a19995c96d15e25d358
Den <denmen0207@gmail.com>
2021-03-06 14:01:54 +0200

    #233. Omit flicking of epoxy model when fetch all ratingChanges.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/users/UserItem.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/users/UserItem.kt
@@ -38,6 +38,9 @@ data class UserItem(private val user: io.xorum.codeforceswatcher.features.users.
         val dateFormat = SimpleDateFormat(CwApp.app.getString(R.string.user_date_format), Locale.getDefault())
         return dateFormat.format(Date(seconds * 1000)).toString()
     }
+
+    // Needed for disable flicking of epoxy model when all ratingChanges fetched
+    override fun toString() = "$avatarLink$update$handle$rating$lastRatingUpdate$dateOfLastRatingUpdate$rankColor"
 }
 
 fun getColorByUserRank(rank: String?) = when (rank) {
@@ -78,7 +81,7 @@ fun getColorByUserRank(rank: String?) = when (rank) {
 
 fun colorTextByUserRank(text: String, rank: String?) = if (listOf("legendary grandmaster", "легендарный гроссмейстер").contains(rank)) {
     val colorText = "<font color=black>${text[0]}</font><font color=red>${
-    text.subSequence(1, text.length)
+        text.subSequence(1, text.length)
     }</font>"
     SpannableString(HtmlCompat.fromHtml(colorText, HtmlCompat.FROM_HTML_MODE_LEGACY))
 } else SpannableString(text).apply {
