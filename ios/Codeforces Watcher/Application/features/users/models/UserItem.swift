31709af38286ab5ea336c527a150647f0004449f
Ivan Karavaiev <karavaiev.i@gmail.com>
2021-03-10 09:57:18 +0200

    #265. Open 'UserAccountItem'.

--- ios/Codeforces Watcher/Application/features/users/models/UserItem.swift
+++ ios/Codeforces Watcher/Application/features/users/models/UserItem.swift
@@ -39,7 +39,36 @@ enum UserItem {
         }
     }
     
+    struct UserAccountItem {
+        let id: Int
+        let avatar: String
+        let rank: String?
+        let handle: String
+        let rating: Int?
+        let maxRating: Int?
+        let firstName: String?
+        let lastName: String?
+        let maxRank: String?
+        let ratingChanges: [RatingChange]
+        let contribution: Int?
+        
+        init(_ user: User) {
+            id = Int(user.id)
+            avatar = user.avatar
+            rank = user.rank
+            handle = user.handle
+            rating = user.rating as? Int
+            maxRating = user.maxRating as? Int
+            firstName = user.firstName
+            lastName = user.lastName
+            maxRank = user.maxRank
+            ratingChanges = user.ratingChanges
+            contribution = user.contribution as? Int
+        }
+    }
+    
     case loginItem
     case verifyItem
     case userItem(UserItem)
+    case userAccount(UserAccountItem)
 }
